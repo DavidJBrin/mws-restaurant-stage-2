acf6ab6ef029222b0260562d36e36236bdfb15c9
David J Brin <justin@justinraith.com>
2018-09-13 16:27:00 -0500

    Establish and populate DB through idb

--- js/dbhelper.js
+++ js/dbhelper.js
@@ -2,15 +2,85 @@
  * Common database helper functions.
  */
 
-function openDatabase() {
-  if (!navigator.serviceWorker){
-    return Promise.resolve();
+ /* 
+ Included idb.js as a local file and included the building of the database in the dbhelper file rather than the service worker. While this isn't ideal for production,
+ it was the easiest method for me to follow.
+ The following resources were consulted to help build the database creation code:
+ https://developer.mozilla.org/en-US/docs/Web/API/IndexedDB_API/Using_IndexedDB
+ https://developers.google.com/web/ilt/pwa/working-with-indexeddb
+ */
+
+var idbProject = (function() {
+  'use strict';
+
+  //is indexedDB supported; if not throw an error
+  if(!('indexedDB' in window)) {
+    console.log('indexedDB not supported in this browser');
+    return;
   }
-  return idb.open('raith-udacity-restaurant', 1, function(updgradeDb) {
-    var restStore = upgradeDb.createObjectStore('restaurants', {keyPath: 'id'});
-    restStore.createIndex('neighborhood', 'neighborhood');
+
+  // initiate DB magic and mumbojumbo
+  var dbPromise = idb.open('brin-restaurant-review-stage2', 1, function(upgradeDB) {
+    switch (upgradeDB.oldVersion) {
+      case 0:
+      case 1:
+        console.log('Establishing object store for the project');
+        upgradeDB.createObjectStore('restaurants', {keyPath: 'id'});
+    }
   });
-};
+
+  // All the restaurants into the pool
+  function addRestaurants() {
+    fetch(DBHelper.DATABASE_URL)
+    .then(response => response.json())
+    .then(function(restaurants) {
+      console.log('json data for restaurants compiled')
+      dbPromise.then((db) => {
+        let restaurantValStore = db.transaction('restaurants', 'readwrite').objectStore('restaurants')
+          for (const restaurant of restaurants) {
+            restaurantValStore.put(restaurant)
+          }
+      })
+      //send the information back out
+      callback(null, restaurants);
+    }).catch(function (err) {
+      dbPromise.then( (db) => {
+        let restaurantValStore = db.transaction('restaurants').objectStore('restaurants')
+        return restaurantValStore.getAll();
+      })
+    })
+  }
+
+  //get the restaurants using the id field for the identifier
+  function getByID(id) {
+    return dbPromise.then(function(db) {
+      const txtion = db.transaction('restaurants', 'readonly');
+      const store = txtion.objectStore('restaurants');
+      return store.get(parseInt(id));
+    }).then(function(restaurantObject) {
+      return restaurantObject;
+    }).catch(function(z) {
+      console.log('Fetch Function errored out:', z);
+    });
+  }
+
+  //retrieve the restaurants like a champ
+  function getRestaurantsAll() {
+    dbPromise.then(db => {
+      return db.transaction('restaurants').objectstore('restaurants')
+      .getRestaurantsAll();
+    }).then(allObjs => console.log(allObjs));
+  }
+
+  //send back the promises 
+  return {
+    dbPromise: (dbPromise),
+    addRestaurants: (addRestaurants),
+    getByID: (getByID),
+    getRestaurantsAll: (getRestaurantsAll)
+  };
+   
+})();
 
 class DBHelper {
 
